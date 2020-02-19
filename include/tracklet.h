96e2a828b38abd3c91b9576fb2eeff46a1e3e364
YeungLy <yangly29@mail2.sysu.edu.cn>
2020-02-19 21:10:50 +0800

    finishing include file

--- include/tracklet.h
+++ include/tracklet.h
@@ -9,28 +9,39 @@ class DetectedBox
 {
     public:
     
-    double x, y, z; 
-    double l, w, h;
-    double theta;
+
     DetectedBox();
-    DetectedBox(double x, double y, double z, double l, double w, double h, double theta)
+    DetectedBox(double x, double y, double z, double l, double w, double h, double yaw): 
+        x_(x), y_(y), z_(z), l_(l), w_(w), h_(h), yaw_(yaw) {}
+
+    double x_, y_, z_; 
+    double l_, w_, h_;
+    double yaw_;
+    
+    bool is_matched_ = false; 
+    
+
+};
+        
 
 class Tracklet
 {
     public:
 
     Tracklet();
-    Tracklet(DetectedBox det, int startframe=0);
+    Tracklet(DetectedBox det, int start_frame): start_frame_(start_frame){
+        dets.clear();
+        dets_.push_back(det);
+    }
     
+    std::vector<DetectedBox> dets_;   // (x, y, z, l, w, h, theta, )
+    int start_frame_;
+    int duration_;
+    std::vector<double> params_; //(vx, vy, vz) ?
     
-    std::vector<DetectedBox> dets;   // (x, y, z, l, w, h, theta, )
-    int start_frame;
-    int duration;
-    std::vector<double> params; //(vx, vy, vz) ?
-    
-    bool matched;  //default is true.
+    bool is_matched_;  //default is true.
 
-}
+};
 
 class TrackletManager
 {
@@ -40,7 +51,10 @@ class TrackletManager
    //unmatched : assign the bool value to unmatch
     void UpdateTracklet(std::vector<DetectedBox> curr_dets);
     void Association(std::vector<DetectedBox> curr_dets);
+    void AddTracklet(DetectedBox first_box, int start_frame);
+    
     //if add or if delete?
     //member variable 
-    std::vector<Tracklet> tracklets;
+    std::map<uint32_t, Tracklet> tracklets;
+    uint32_t max_id;
 }
