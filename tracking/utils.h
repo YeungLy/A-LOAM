0cbfcbc799b2973cb4147fdc0d2b24de3929c6cb
YeungLy <yangly29@mail2.sysu.edu.cn>
2020-03-02 11:36:08 +0800

    utils.h: add box to 8 corners

--- tracking/utils.h
+++ tracking/utils.h
@@ -1,8 +1,12 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include <vector>
 #include "tracklet.h"
+
+#include <cmath>
+#include <vector>
+
+#include <Eigen/Dense.h>
 #include <jsk_recognition_msgs/BoundingBox.h>
 #include <jsk_recognition_msgs/BoundingBoxArray.h>
 //IOU computation.. 
@@ -44,6 +48,33 @@ std::vector<DetectedBox> RosMsgToBoxes(jsk_recognition_msgs::BoundingBoxArrayCon
 }
 
 //box3d to 8corners
+Eigen::MatrixXd Box3dToCorners(const DetectedBox & box)
+{
+    //8corners: (x,y,z), 3*8
+    Eigen::MatrixXd corners(3, 8);
+    corners << box.l / 2., -box.l / 2., box.l / 2., -box.l / 2., box.l / 2., -box.l / 2., box.l / 2., -box.l / 2.,
+              -box.w / 2., -box.w / 2., box.w /2., box.w / 2., -box.w / 2., -box.w / 2., box.w /2., box.w / 2.,
+              0, 0, 0, 0, box.h, box.h, box.h, box.h;
+    
+    Eigen::Quaterniond q{std::cos(box.yaw/2.), 0.0, 0.0, std::sin(box.yaw/2.)};
+    Eigen::Vector3d t(box.x, box.y, box.z);
+    Eigen::MatrixXd t_mat(3, 8);
+    t_mat << t, t, t, t, t, t, t, t;
+    corners = corners * q + t_mat;
+    return corners;
+}
+
+double BoxIoUBev(const Eigen::MatrixXd & icorners, const Eigen::MatrixXd & jcorners)
+{
+    double iou;
+
+    double iarea = std::sqrt((icorners(0, 0) - icorners(0, 1)*(icorners(0, 0) - icorners(0, 1) * (icorners(1, 0) - icorners(1, 1)*(icorners(1, 0) - icorners(1, 1));
+    double jarea = std::sqrt((jcorners(0, 0) - jcorners(0, 1)*(jcorners(0, 0) - jcorners(0, 1) * (jcorners(1, 0) - jcorners(1, 1)*(jcorners(1, 0) - jcorners(1, 1));
+    double union = iarea + jarea;
+    double inter;
+    return iou;
+    
+}
 
 //8corners to box3d
 
